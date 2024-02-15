41e4ae2a7be86ad71645203b021451a9c8850afa
Aidarchik <ishimgrad@gmail.com>
2024-02-16 01:38:23 +0500

    response edit

--- functions/api.js
+++ functions/api.js
@@ -3,7 +3,7 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.send("App is running..");
+  res.send("App is running..!!!!");
 });
 app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);
