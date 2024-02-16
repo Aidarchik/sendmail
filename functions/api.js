1dc4ae8ae397bfb9d35141eeb7cc8da23348f834
Aidarchik <ishimgrad@gmail.com>
2024-02-16 20:50:57 +0500

    Add NodeMailer

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mail.ru",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "aidar.mustaev@mail.ru",
+    pass: "nLRU5AutWap0RGnnqTFz",
+  },
+});
+
+async function main() {
+  try {
+    const info = await transporter.sendMail({
+      from: "aidar.mustaev@mail.ru",
+      to: "giper.kubik@yandex.ru",
+      subject: "Hello ✔",
+      text: "Hello world?",
+      html: "<b>Hello world?</b>",
+    });
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
 const app = express();
 const router = express.Router();
 router.get("/", (req, res) => {
+  main();
   res.send("App is running..!!!!");
 });
 router.get("/hello", (req, res) => {
@@ -11,6 +39,6 @@ router.get("/hello", (req, res) => {
 app.use("/", router);
 module.exports.handler = serverless(app);
 // const port = 8080;
-//app.listen(process.env.PORT || port, () => {
-//	console.log(`Listening on port ${port}`);
-//});
+// app.listen(process.env.PORT || port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
